4f24a78c565062e964534c873b8ab6e048da12be
mschutzner <matthewschutzner@gmail.com>
2023-09-10 15:16:26 -0700

    Updated for hosting on schutzner.org

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import { Outlet } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import HeaderFooter from './components/HeaderFooter';
+import NotFound from './components/NotFound';
+import Checkout from './components/Checkout';
+import Cart from './components/Cart';
+import Products from './components/Products';
+import Home from './components/Home';
+import ProductPage from './components/ProductPage';
+import About from './components/About';
+import { CartProvider } from './contexts/CartContext';
+import Success from './components/Success';
+import Search from './components/Search';
 
 function App() {
   return (
-    <div className="App">
-      <HeaderFooter>
-        <Outlet/>
-      </HeaderFooter>
-    </div>
+    <CartProvider>
+      <Router basename="/ecommerce">
+        <HeaderFooter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="products" element={<Products/>} />
+            <Route path="products/:id" element={<ProductPage />} />
+            <Route path="cart" element={<Cart/>} />
+            <Route path="checkout" element={<Checkout/>} />
+            <Route path="success" element={<Success />} />
+            <Route path="search/:search" element={<Search />} />
+            <Route path="*" element={<NotFound/>} />
+          </Routes>
+        </HeaderFooter>
+      </Router>
+    </CartProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,38 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NotFound from './components/NotFound';
-import Checkout from './components/Checkout';
-import Cart from './components/Cart';
-import Products from './components/Products';
-import Home from './components/Home';
-import ProductPage from './components/ProductPage';
-import About from './components/About';
-import { CartProvider } from './contexts/CartContext';
-import Success from './components/Success';
-import Search from './components/Search';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CartProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route index element={<Home/>} />
-            <Route path="about" element={<About />} />
-            <Route path="products" element={<Products/>} />
-            <Route path="products/:id" element={<ProductPage />} />
-            <Route path="cart" element={<Cart/>} />
-            <Route path="checkout" element={<Checkout/>} />
-            <Route path="success" element={<Success />} />
-            <Route path="search/:search" element={<Search />} />
-            <Route path="*" element={<NotFound/>} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </CartProvider>
+      <App />
   </React.StrictMode>
 );
 
